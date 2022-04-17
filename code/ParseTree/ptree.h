8f251a87b16c32ce27af29ab3b754a496bd24513
Jontom01 <67528141+Jontom01@users.noreply.github.com>
2022-04-17 11:07:19 -0400

    Update ptree.h
    New implementation needed since node struct data was switched for char to void* to allow for more than one data type to be stored.

--- code/ParseTree/ptree.h
+++ code/ParseTree/ptree.h
@@ -5,23 +5,25 @@
 
 /**
 Node struct to hold necessary data for a 3 branch parse tree implementation
-@member data - holds the value for the terminal
+@member data - holds pointer to the value for the terminal
 @member exp - true if the node represents an expression instead of a terminal
 @member left - pointer to left branch node
 @member mid - pointer to middle branch node
-@member right - pointer to right branch node
+@member right - pointer to right branch
 **/
 typedef struct node node;
 
 struct node{
-    char data;
+    void *data;
     bool exp;
     node *left;
     node *mid;
     node *right;
 };
 
-node *init_ptree();
+node *init_term_node(char val);
+
+node *init_exp_node();
 
 node *insert(node *n, char left, char mid, char right);
 
