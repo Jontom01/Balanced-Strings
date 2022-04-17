6bdc1827054799a2fc4a65b70c9994e0cffd8d0a
Jontom01 <67528141+Jontom01@users.noreply.github.com>
2022-04-16 20:56:04 -0400

    Update balanced.c

--- code/balanced.c
+++ code/balanced.c
@@ -4,16 +4,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include "RDparser.h"
+#include "ptree.h"
 
 int main(int argc, char* argv[]) {
     assert(argv[1]); //to hold the terminal characters of the grammar
     assert(argv[2]); //to hold the input string
     token *tok = init_tokens(argv[2]);
+    node *ptree = init_ptree();
     if(!tok){
         fprintf(stderr, "could not allocate memory to token members");
         exit(1);
     }
-    parse(tok, argv[1]);
+    parse(tok, argv[1], ptree);
+    traverse(ptree); //prints the parse tree 
+    printf("\n");
+    free_ptree(ptree);
     free(tok);
     return 0;
 }
