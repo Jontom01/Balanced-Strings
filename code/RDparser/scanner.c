41a60a97b4d39a55c0c19447d6ee44c9911b7371
Jontom01 <67528141+Jontom01@users.noreply.github.com>
2022-04-27 13:05:05 -0400

    Update and rename RDparser.c to scanner.c

--- code/RDparser/scanner.c
+++ code/RDparser/scanner.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "RDparser.h"
+#include "scanner.h"
 #include "ptree.c"
 
 //Recursive Descent Parser Implentation for Balanced-Strings
